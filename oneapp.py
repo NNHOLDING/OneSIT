9b45e0062d781adafd0f69b2b5ca3455632af10d
NNHOLDING <nholdingsolutions@gmail.com>
2025-08-20 21:39:09 -0600

    Update oneapp.py

--- oneapp.py
+++ oneapp.py
@@ -130,6 +130,19 @@ if st.session_state.logueado_handheld:
 
             st.subheader("📑 Registros")
             st.dataframe(df_filtrado)
+            #📈 Actividad del Usuario por Fecha
+           st.subheader("📈 Actividad del Usuario por Fecha")
+           if not df_filtrado.empty:
+            actividad_por_fecha = (
+                df_filtrado.groupby(df_filtrado["fecha"].dt.date)
+                .size()
+                .reset_index(name="Registros")
+            )
+            actividad_por_fecha = actividad_por_fecha.sort_values("fecha")
+            st.line_chart(actividad_por_fecha.set_index("fecha"))
+           else:
+            st.info("ℹ️ No hay registros para el usuario y rango de fecha seleccionados.")
+
 
                         # ✅ Tabla de registros entregados y devueltos hoy
             hoy = datetime.now(cr_timezone).date()
@@ -210,6 +223,7 @@ st.markdown("""
         NN HOLDING SOLUTIONS, Ever Be Better &copy; 2025, Todos los derechos reservados
     </div>
 """, unsafe_allow_html=True)
+
 
 
 
