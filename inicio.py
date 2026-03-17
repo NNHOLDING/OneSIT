5a7f21d6895ba114d29282c4f7637f5e82cfdea7
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-17 00:31:16 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -2,16 +2,18 @@ import streamlit as st
 
 def mostrar_inicio():
     st.markdown("""
-        <div style='text-align: center; margin-top: 50px;'>
+        <div style='text-align: center; margin-top: 60px;'>
             <h1 style='
                 font-family: Arial Black, sans-serif;
-                font-size: 72px;
+                font-size: 80px;
                 font-weight: 900;
-                color: #006699;
-                text-shadow: 3px 3px 0px #ffffff, 
-                             -3px -3px 0px #ffffff,
-                             3px -3px 0px #ffffff,
-                             -3px 3px 0px #ffffff;'>
+                color: #0066cc;
+                letter-spacing: 4px;
+                text-shadow: 
+                    -3px -3px 0 #ffffff,  
+                    3px -3px 0 #ffffff,
+                    -3px 3px 0 #ffffff,
+                    3px 3px 0 #ffffff;'>
                 WMS SIT
             </h1>
         </div>
