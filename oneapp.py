5fa11ffbda04124c6024e7d7a19418fb1e90d348
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-16 19:00:35 -0600

    Update oneapp.py

--- oneapp.py
+++ oneapp.py
@@ -28,6 +28,7 @@ from modulo_consulta_sku import mostrar_consulta_sku
 from modulo_reporte import mostrar_reporte
 from defaults import defaults
 from estilos import aplicar_estilos
+from bitacora import registrar_evento
 
 
 # Configuración de página
