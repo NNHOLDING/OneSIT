5cd81cb01988171dc08d22478bfe99e1d7e1a743
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-21 09:49:42 -0600

    Update oneapp.py

--- oneapp.py
+++ oneapp.py
@@ -273,7 +273,7 @@ st.markdown("""
     <hr style="margin-top: 50px; border: none; border-top: 1px solid #ccc;" />
     <div style="
         text-align: center; 
-        background-color: #f5f5f5; 
+        background-color: #d0d0d0;  /* mismo color que la etiqueta */
         padding: 15px; 
         border-radius: 8px; 
         color: #000000; 
