0660a40e0f24c37ba091e3f23ec9deb958cc0a9a
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-21 10:12:27 -0600

    Update footer.py

--- footer.py
+++ footer.py
@@ -1,15 +1,17 @@
-# 🧾 Footer institucional
-st.markdown("""
-    <hr style="margin-top: 50px; border: none; border-top: 1px solid #ccc;" />
-    <div style="
-        text-align: center; 
-        background-color: #d0d0d0;  /* mismo color que la etiqueta */
-        padding: 15px; 
-        border-radius: 8px; 
-        color: #000000; 
-        font-size: 1em; 
-        margin-top: 20px;">
-        <strong>Powered by NN HOLDING SOLUTIONS, Ever Be Better &copy; 2016-2026</strong><br>
-        Todos los derechos reservados
-    </div>
-""", unsafe_allow_html=True)
+import streamlit as st
+
+def mostrar_footer():
+    st.markdown("""
+        <hr style="margin-top: 50px; border: none; border-top: 1px solid #ccc;" />
+        <div style="
+            text-align: center; 
+            background-color: #d0d0d0;  /* mismo color que la etiqueta */
+            padding: 15px; 
+            border-radius: 8px; 
+            color: #000000; 
+            font-size: 1em; 
+            margin-top: 20px;">
+            <strong>Powered by NN HOLDING SOLUTIONS, Ever Be Better &copy; 2016-2026</strong><br>
+            Todos los derechos reservados
+        </div>
+    """, unsafe_allow_html=True)
