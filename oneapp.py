8d223d59f66038b9f3fc29130e1c7fc139330526
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-21 10:09:55 -0600

    Update oneapp.py

--- oneapp.py
+++ oneapp.py
@@ -33,6 +33,7 @@ from inicio import mostrar_inicio
 from navbar import mostrar_navbar
 import streamlit as st
 from usuario_info import mostrar_usuario_info
+from footer import mostrar_footer
 
 
 # Configuración de página
@@ -269,17 +270,4 @@ if st.session_state.logueado_handheld:
             st.session_state[key] = False
 
 # 🧾 Footer institucional
-st.markdown("""
-    <hr style="margin-top: 50px; border: none; border-top: 1px solid #ccc;" />
-    <div style="
-        text-align: center; 
-        background-color: #d0d0d0;  /* mismo color que la etiqueta */
-        padding: 15px; 
-        border-radius: 8px; 
-        color: #000000; 
-        font-size: 1em; 
-        margin-top: 20px;">
-        <strong>Powered by NN HOLDING SOLUTIONS, Ever Be Better &copy; 2016-2026</strong><br>
-        Todos los derechos reservados
-    </div>
-""", unsafe_allow_html=True)
+mostrar_footer()
