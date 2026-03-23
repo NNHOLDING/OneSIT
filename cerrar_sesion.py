6a8beb7bbcb800c7f48806519c15ab8ee84bf44d
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-22 23:20:47 -0600

    Update cerrar_sesion.py

--- cerrar_sesion.py
+++ cerrar_sesion.py
@@ -2,16 +2,12 @@ import streamlit as st
 from defaults import defaults
 
 def cerrar_sesion(registrar_log):
-    # Registrar cierre de sesión en LogEnvios
     registrar_log(
         st.session_state.codigo_empleado,
         st.session_state.nombre_empleado,
         "Login",
         "Cierre de sesión"
     )
-
-    # Reiniciar variables de sesión con los valores por defecto
     for key, value in defaults.items():
         st.session_state[key] = value
-
     st.success("✅ Sesión cerrada correctamente")
