7a28a7e5c6a1526b899ca67679c6042b6d8d48ed
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-23 19:39:33 -0600

    Add files via upload

--- registroins.py
+++ registroins.py
@@ -7,23 +7,26 @@ from google.oauth2.service_account import Credentials
 from geopy.geocoders import Nominatim
 from streamlit_geolocation import geolocation
 
+# Configuración de zona horaria
 cr_timezone = pytz.timezone("America/Costa_Rica")
 
 def panel_registro():
-    # Conexión con Google Sheets
+    # Conexión con Google Sheets usando st.secrets
     scope = ["https://spreadsheets.google.com/feeds",
              "https://www.googleapis.com/auth/drive"]
+
     creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
     client = gspread.authorize(creds)
     sheet = client.open_by_key("1PtUtGidnJkZZKW5CW4IzMkZ1tFk9dJLrGKe9vMwg0N0").worksheet("INS")
 
+    # Interfaz Streamlit
     st.title("📝 Registro INS")
 
     fecha = st.date_input("Fecha", datetime.datetime.now(cr_timezone).date())
     hora = st.time_input("Hora", datetime.datetime.now(cr_timezone).time())
     numero_evento = st.text_input("Número de evento")
 
-    # Geolocalización desde el dispositivo
+    # 👉 Geolocalización desde el dispositivo
     location = geolocation()
     lat, lon = None, None
     provincia, canton, distrito = "", "", ""
@@ -31,6 +34,7 @@ def panel_registro():
         lat = location["latitude"]
         lon = location["longitude"]
 
+        # Reverse geocoding con Nominatim
         geolocator = Nominatim(user_agent="geoapi")
         loc = geolocator.reverse(f"{lat}, {lon}")
         if loc:
