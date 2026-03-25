744d39e6ec0168976cef828b7d9cf06cb6bebcc1
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-24 19:45:35 -0600

    Add files via upload

--- registroins.py
+++ registroins.py
@@ -7,6 +7,22 @@ cr_timezone = pytz.timezone("America/Costa_Rica")
 def panel_registro():
     st.title("🕒 Registro INS")
 
+    # Bloque de estilo para fondo oscuro y texto claro
+    st.markdown(
+        """
+        <style>
+        .stSelectbox div[data-baseweb="select"] {
+            background-color: #000000 !important;
+            color: #FFFFFF !important;
+            font-size: 18px !important;
+        }
+        .stSelectbox svg { fill: #FFFFFF !important; }
+        .stSelectbox span { color: #FFFFFF !important; }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     # Lista de horas en intervalos de 30 minutos
     horas = [f"{h:02d}:{m:02d}" for h in range(0,24) for m in (0,30)]
     hora_actual = datetime.datetime.now(cr_timezone).strftime("%H:%M")
