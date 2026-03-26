4faf0aba4e578eebb4c0a5f5e5d5e97e6cfcd018
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-25 22:06:29 -0600

    Add files via upload

---
+++ inactividad.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+def cerrar_sesion():
+    from defaults import defaults
+    from oneapp import registrar_log  # ajusta el import según tu estructura
+
+    registrar_log(st.session_state.codigo_empleado,
+                  st.session_state.nombre_empleado,
+                  "Login",
+                  "Cierre de sesión")
+
+    for key in defaults.keys():
+        st.session_state[key] = False
+
+def script_inactividad(minutos=5):
+    st.markdown(f"""
+    <script>
+    let timeout;
+    function resetTimer() {{
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {{
+            window.location.href = "/?expired=true"; 
+        }}, {minutos*60000});
+    }}
+    window.onload = resetTimer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+    </script>
+    """, unsafe_allow_html=True)
