72d9ddff046a87f2ae049331df59381d2bc9d00c
NNHOLDING <nholdingsolutions@gmail.com>
2026-03-25 22:38:04 -0600

    Update inactividad.py

--- inactividad.py
+++ inactividad.py
@@ -1,3 +1,17 @@
+import streamlit as st
+
+def cerrar_sesion():
+    from defaults import defaults
+    from oneapp import registrar_log  # ajusta este import si registrar_log está en otro módulo
+
+    registrar_log(st.session_state.codigo_empleado,
+                  st.session_state.nombre_empleado,
+                  "Login",
+                  "Cierre de sesión")
+
+    for key in defaults.keys():
+        st.session_state[key] = False
+
 def script_inactividad(minutos=5):
     st.markdown(f"""
     <script>
